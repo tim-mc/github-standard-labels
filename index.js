2cd0efc1ea1269157c1ea76123989b0d241ce2b0
Tim McClure <tmcclure@workmarket.com>
2017-03-03 08:24:58 -0500

    Adding wm labels

--- index.js
+++ index.js
@@ -2,25 +2,19 @@ var mapLimit = require('map-limit')
 var request = require('request')
 
 var colors = {
-  'duplicate': 'ededed',
-  'greenkeeper': 'ededed',
-  'starter': 'ffc0cb',
-  'Priority: Critical': 'ee0701',
-  'Priority: High': 'd93f0b',
-  'Priority: Low': '0e8a16',
-  'Priority: Medium': 'fbca04',
-  'Status: Abandoned': '000000',
-  'Status: Available': 'c2e0c6',
-  'Status: Blocked': 'ee0701',
-  'Status: In Progress': 'cccccc',
-  'Status: On Hold': 'e99695',
-  'Status: Proposal': 'd4c5f9',
-  'Status: Review Needed': 'fbca04',
-  'Type: Bug': 'ee0701',
-  'Type: Documentation': '5319e7',
-  'Type: Enhancement': '1d76db',
-  'Type: Maintenance': 'fbca04',
-  'Type: Question': 'cc317c'
+  'Breaking/Major': '5319e7',
+  'Bug': 'b60205',
+  'Documentation': 'dddddd',
+  'Feature/Minor': '1d76db',
+  'Feature Request': 'd4c5f9',
+  'Fix/Patch': 'c5def5',
+  'HELP': 'e99695',
+  'New': 'fbca04',
+  'NO RELEASE': 'fef2c0',
+  'Not Ready': 'd93f0b',
+  'Questions': '000000',
+  'RELEASE': 'ffffff',
+  'Urgent™/Blocker™': '006b75',
 }
 
 module.exports = githubStandardLabels
